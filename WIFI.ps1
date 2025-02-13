0ba4e31a77e9bd42a2043e4d735e06f2acbe210e
guilherme-luc <152202121+guilherme-luc@users.noreply.github.com>
2025-02-13 10:44:15 -0300

    Update WIFI.ps1

--- WIFI.ps1
+++ WIFI.ps1
@@ -1,15 +1,4 @@
-# Esconde a janela do PowerShell
-Add-Type -Name Window -Namespace Console -MemberDefinition '
-    [DllImport("Kernel32.dll")] 
-    public static extern IntPtr GetConsoleWindow();  
-    [DllImport("User32.dll")] 
-    public static extern bool ShowWindow(IntPtr hWnd, int nCmdShow); 
-'
 
-$consolePtr = [Console.Window]::GetConsoleWindow()
-[Console.Window]::ShowWindow($consolePtr, 0)  # 0 = Oculto, 5 = Visível
-
-# A partir daqui, o restante do script pode ser executado normalmente
 set-ExecutionPolicy unrestricted -Force
 Set-WinUserLanguageList en-US -Force
 net start wlansvc
